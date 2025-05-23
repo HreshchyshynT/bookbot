790fe2e4d4f4ecf800e644592e45f5c8d1003a58
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-05-24 00:19:22 +0300

    add methods for characters count and pretty output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_characters, get_sorted_characters_by_count
 
 
 def get_book_text(filepath: str) -> str:
@@ -7,9 +7,20 @@ def get_book_text(filepath: str) -> str:
 
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    book_path = "./books/frankenstein.txt"
+    book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
-    print(f"{num_words} words found in the document")
+    num_characters = get_num_characters(book_text)
+    sorted_characters = get_sorted_characters_by_count(num_characters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_characters:
+        if type(item["char"]) == str and item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,23 @@
 def get_num_words(text: str) -> int:
     return len(text.split())
+
+
+def get_num_characters(text: str) -> dict[str, int]:
+    result = {}
+    for c in text:
+        lower_c = c.lower()
+        if lower_c in result:
+            result[lower_c] += 1
+        else:
+            result[lower_c] = 1
+    return result
+
+
+def get_sorted_characters_by_count(
+    chars_num: dict[str, int],
+) -> list[dict[str, object]]:
+    result = []
+    for c, num in chars_num.items():
+        result.append({"char": c, "num": num})
+    result.sort(reverse=True, key=lambda d: d["num"])
+    return result
