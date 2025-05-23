9fa1249f0d66f439ebae1e5ab0136eb33d536eae
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-05-24 00:25:01 +0300

    add read path from args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_characters, get_sorted_characters_by_count
+import sys
 
 
 def get_book_text(filepath: str) -> str:
@@ -7,7 +8,12 @@ def get_book_text(filepath: str) -> str:
 
 
 def main():
-    book_path = "./books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     num_characters = get_num_characters(book_text)
